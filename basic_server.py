e434da6956354e526d81d8ee2af7841e09801831
Stephen <stephenlang91@gmail.com>
2025-01-26 15:27:11 -0500

    initial commit

---
+++ basic_server.py
@@ -0,0 +1,21 @@
+import socket
+
+song = """
+Heeey HeeeEEEEyyyyy Baaaby!
+Ooooooooh! Aaaaaaah!
+I want to knoOOOooOOO will you be my girl?
+"""
+
+HOST, PORT = '', 8888
+
+# Create a socket
+listen_socket = 
+
+
+# Bind
+
+# listen
+
+# accept
+
+
