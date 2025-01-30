cce1fa07a7b2cc136269e638ca07ebbdb304b85e
Stephen <stephenlang91@gmail.com>
2025-01-29 23:01:19 -0500

    refactored basic server to use Song module

--- basic_server.py
+++ basic_server.py
@@ -1,16 +1,8 @@
 import socket
+from Song import Song
 
-song = """
-Heeey HeeeEEEEyyyyy Baaaby!
-Ooooooooh! Aaaaaaah!
-I want to knoOOOooOOO will you be my girl?
-"""
-
+song = Song()
 HOST, PORT = '', 8888
-
-def gen_lines():
-    for line in song.split('\n'):
-        yield line
 
 # Create a socket
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,17 +12,17 @@ listen_socket.bind((HOST, PORT))
 # listen
 listen_socket.listen(1)
 print(f'serving HTTP on port {PORT}')
-lines = gen_lines()
 
 # accept
 while True:
     client_connection, client_address = listen_socket.accept()
     request_data = client_connection.recv(1024)
+    line = song.next_line()
 
     template = f"""\
 HTTP/1.1 200 OK
 
-{next(lines)}
+{line}
 """
     http_response =  bytes(template, 'utf-8')
     client_connection.sendall(http_response)
